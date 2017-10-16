82a0099e5c446348692a2ba40ffbba980c6b8898
Luis Reisewitz <reisewitz@me.com>
2017-10-16 16:09:56 +0200

    Update Package.swift to SPM4 format

--- Package.swift
+++ Package.swift
@@ -3,11 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "StencilSwiftKit",
+    products: [
+        .library(name: "StencilSwiftKit", targets: ["StencilSwiftKit"])
+    ],
     dependencies: [
         .package(url: "https://github.com/zweigraf/Stencil.git", .branch("master")),
     ],
     targets: [
-        .target(name: "StencilSwiftKit", dependencies: ["Stencil"])
+        .target(name: "StencilSwiftKit", dependencies: ["Stencil"], path: "Sources")
     ],
     swiftLanguageVersions: [4]
 )
